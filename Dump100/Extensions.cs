bb27064e6d63182494d1bfff3f1aab5ed05d4c68
Max <mr.script@outlook.com>
2020-06-06 21:13:58 +0300

    sample projects add

---
+++ Dump100/Extensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace Dump100
+{
+    public static  class Extensions
+    {
+        //private static TextWriter _dump = LINQPad.Util.CreateXhtmlWriter();
+        //public static void Dump<T>(this T o)
+        //{
+        //    var html = "";
+        //    string localUrl = Path.GetTempFileName() + ".html";
+        //    _dump.Write(o);
+        //    _dump.ToString();
+        //    //using (var writer = LINQPad.Util.CreateXhtmlWriter(true))
+        //    //{
+        //    //    writer.Write(o);
+        //    //    html = writer.ToString();
+        //    //    Debug.Write(writer.ToString());
+        //    //    //File.WriteAllText(localUrl, writer.ToString());
+        //    //}
+        //    ////Process.Start(localUrl);
+        //}
+
+    }
+}
---
+++ Dump100/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dump100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            "Hello".Dump();
+            List<string> list = new List<string>();
+            list.Add("abc");
+            list.Add("111");
+            list.Dump();
+        }
+    }
+}
